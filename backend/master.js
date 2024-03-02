991dfcfb490e3b36ffda306304253459b315c386
aarumugapandi <aarumugapandi762004@gmail.com>
2024-03-02 09:35:30 +0530

    mine

---
+++ backend/master.js
@@ -0,0 +1,124 @@
+import mongoose from 'mongoose';
+import { TotalStudents,StudentSchemaModel,DB } from '../server.js';
+
+const master = async (req, res) => {
+    mongoose.connect("mongodb://0.0.0.0/Student_Management")
+    // const pattern = /^[a-zA-Z](20|21|22|23)[a-zA-Z]{3,6}\d{3,5}(L)?$/
+    const patternForY = /^([a-zA-Z]+)(20|21|22|23)([a-zA-Z]{2,6})(\d{3,5})(L)?$/;
+    let FilteredTotalStudents = TotalStudents.filter(row => patternForY.test(row.Register_No))
+    let fake = TotalStudents.filter(row => !patternForY.test(row.Register_No))
+
+    FilteredTotalStudents.forEach(studentInfo => {
+        const match = studentInfo.Register_No.match(patternForY)
+        if (match) {
+            switch (match[1]) {
+                case "E":
+                    studentInfo.Institution = "Engineering"; break
+
+                case "P":
+                    studentInfo.Institution = "Pharmacy"; break
+
+                case "A":
+                    studentInfo.Institution = "Allied Health and Science"; break
+
+                case "N":
+                    studentInfo.Institution = "Nursing"; break
+            }
+            switch (parseInt(match[2])) {
+                case 20:
+                    studentInfo.Year = 4;
+                    break
+                case 21:
+                    studentInfo.Year = 3;
+                    break
+                case 22:
+                    studentInfo.Year = 2;
+                    break
+                case 23:
+                    studentInfo.Year = 1;
+                    break
+            }
+            switch (match[3]) {
+                case "DP":
+                    studentInfo.Department = "D-Pharm"; break
+                case "BP":
+                    studentInfo.Department = "B-Pharm"; break
+                case "HI":
+                    studentInfo.Department = "Health Inspector"; break
+                case "PD":
+                    studentInfo.Department = "Pharm D"; break
+                case "NS":
+                    studentInfo.Department = "Nursing"; break
+                case "CPPCT":
+                    studentInfo.Department = "CPPCT"; break
+                case "ACT":
+                    studentInfo.Department = "CT"; break
+                case "SCT":
+                    studentInfo.Department = "CT"; break
+                case "MLT":
+                    studentInfo.Department = "MLT"; break
+                case "SOPTO":
+                    studentInfo.Department = "OPTO"; break
+                case "AOT":
+                    studentInfo.Department = "OTAT"; break
+                case "SOT":
+                    studentInfo.Department = "OTAT"; break
+                case "ARIT":
+                    studentInfo.Department = "RIT"; break
+                case "SRIT":
+                    studentInfo.Department = "RIT"; break
+                case "SCPPCT":
+                    studentInfo.Department = "CPPCT"; break
+                case "CY":
+                    studentInfo.Department = "CYBER SECURITY"; break
+                case "AG":
+                    studentInfo.Department = "Agri"; break
+                case "MECS":
+                    studentInfo.Department = "MECS"; break
+                case "ME" || "MC":
+                    studentInfo.Department = "Mech"; break
+                case "BE":
+                    studentInfo.Department = "Bio-Medical"; break
+                case "CS":
+                    studentInfo.Department = "CSE"; break
+                case "EC":
+                    studentInfo.Department = "ECE"; break
+                default:
+                    studentInfo.Department = match[3]
+
+
+            }
+            if (match[1] == "P" && match[5] == "L") {
+                // console.log(match[1], match[5], match[2], studentInfo)
+                switch (match[2]) {
+                    case 20:
+                        studentInfo.Year = 4; break
+                    case 21:
+                        studentInfo.Year = 3; break
+                    case 22:
+                        studentInfo.Year = 2; break
+                    case 23:
+                        studentInfo.Year=1; 
+                }
+            }
+            if (match[1] == "E" && match[5] == "L") {
+                
+                switch (parseInt(match[2])) {
+                    case 20:
+                        studentInfo.Year=5;break
+                    case 21:
+                        studentInfo.Year = 4; break
+                    case 22:
+                        studentInfo.Year = 3; break
+                    case 23:
+                        studentInfo.Year = 2; break
+                }
+            }
+        }
+    })
+    // await StudentSchemaModel.insertMany(FilteredTotalStudents)
+    await DB.collection("Fake").insertMany(fake)
+    res.json(FilteredTotalStudents)
+}
+
+export { master }
---
+++ backend/uploader.js
@@ -0,0 +1,110 @@
+import xlsx from 'xlsx';
+import { DB, Student_ManagementDB } from '../server.js';
+
+const uploader = async (req, res) => {
+    try {
+        const { collection } = req.body;
+        const Collection = DB.collection(String(collection));
+        const file = req.file
+        const fileName = file.originalname
+        const fileBuffer = file.buffer;
+        const workbook = xlsx.read(fileBuffer, { type: 'buffer' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        let data = xlsx.utils.sheet_to_json(sheet, { raw: false });
+
+        const regex = /^([^-]+)-(\d{2}-\d{2}-\d{4})-(\d{2}-\d{2}-[AP]M)\.xlsx$/;
+        // const pattern = /^[a-zA-Z]\d{2,6}[a-zA-Z]{2,6}\d{3,5}(L)?$/
+
+        // Execute the regular expression on the filename
+        const matches = fileName.match(regex);
+        var name
+        if (matches && matches.length === 4) {
+            name = matches[1];
+            const date = matches[2];
+            const time = matches[3];
+        } else {
+            return res.json({ message: "Filename doesn't match the expected pattern." });
+        }
+        if (
+            collection == "Absent" && name == "absent" ||
+            collection == "Present" && name == "present" ||
+            collection == "Holiday" && name == "holiday_students" ||
+            collection == "Partially_Absent" && name == "partially_absent" ||
+            collection == "Dayscholar_Absent" && name == "dayscholar_absent" ||
+            collection == "Dayscholar_Present" && name == "dayscholar_present"
+        ) {
+            // for (let i = 0; i < data.length; i++) {
+
+            //     let rowData = data[i];
+
+            //     let modifiedRowData = {};
+            //     Object.keys(rowData).forEach(key => {
+            //         const modifiedKey = key.replace(/\s+/g, '_');
+            //         modifiedRowData[modifiedKey] = rowData[key];
+            //     });
+
+            //     const pattern = /^[a-zA-Z]\d{2,6}[a-zA-Z]{2,6}\d{3,5}(L)?$/
+
+            //     if (!pattern.test(modifiedRowData['Register_No'])) {
+            //         unknown.push(modifiedRowData['Register_No'])
+            //         continue
+            //     }
+            //     if (modifiedRowData['Register_No']) {
+            //         const foundStudent = await Student_ManagementDB.findOne({ Register_No: modifiedRowData['Register_No'] })
+            //         if (!foundStudent){
+            //             continue
+            //         }
+            //         modifiedRowData['Year']=foundStudent['Year']
+            //     }
+            //     if (modifiedRowData['S_No']) {
+            //         delete modifiedRowData['S_No']
+            //     }
+            //     delete modifiedRowData["Fee_Status"];
+            //     if ((!await Collection.findOne(modifiedRowData) && await Student_ManagementDB.findOne({ Register_No: modifiedRowData["Register_No"] }))) {
+            //         await Collection.insertOne(modifiedRowData);
+            //     } else { 
+            //         count += 1;
+            //     }
+            // }
+            // console.log("Duplicates and Outliers:", count);
+            // res.status(200).json({ status: "Ok" });
+            // console.log(unknown)
+            let count = 0;
+            let unknown = []
+            data.forEach(async rowData => {
+                let modifiedRowData = {};
+                Object.keys(rowData).forEach(key => {
+                    const modifiedKey = key.replace(/\s+/g, '_');
+                    modifiedRowData[modifiedKey] = rowData[key];
+                });
+                const foundStudent = await Student_ManagementDB.findOne({ Register_No: modifiedRowData['Register_No'] })
+                if (foundStudent) {
+                    modifiedRowData['Year'] = foundStudent['Year']
+                    modifiedRowData['Institution'] = foundStudent['Institution']
+                    modifiedRowData['Course'] = foundStudent['Course']
+                    modifiedRowData['Department'] = foundStudent['Department']
+                    modifiedRowData['Gender'] = foundStudent['Gender']
+
+                    if (!Collection.find(modifiedRowData)) {
+                        Collection.insertOne(modifiedRowData)
+                        count+=1
+                    }else{
+                    }
+                } else {
+                    unknown.push(rowData)
+                }
+            })
+            console.log(unknown);
+            return res.json({ status: "Ok" })
+        } else {
+            return res.json({ message: "Filename doesn't match the expected pattern." })
+        }
+
+    } catch (error) {
+        console.error('Error processing file:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export { uploader }
